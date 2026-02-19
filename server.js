b58639bd74357de3d49aed9ea2962483dae5fd71
Peta Gnaneshwar Reddy <petagnaneshwarreddy@gmail.com>
2026-02-19 09:23:28 +0530

    Fix CORS

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
+
 app.use(express.json());
 
 /* =====================================
